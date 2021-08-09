c5fd168ed6df4b963e4b702544ef9f95f4fbb270
Nataly <naesdu@gmail.com>
2021-08-09 16:34:36 -0500

    cambios1

---
+++ script.js
@@ -0,0 +1,8 @@
+// menu
+
+let linksHead = document.querySelectorAll(".linksHead")[0];
+let semaforo = true;
+
+document.querySelectorAll(".burger")[0].addEventListener("click", function(){
+    linksHead.classList.toggle("menudos")
+})
