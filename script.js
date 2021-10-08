05cc5e1480e0b9d26c120b35741edced85ba0207
Nataly1Estupinan <naesdu@gmail.com>
2021-10-07 21:53:03 -0500

    Estilos

--- script.js
+++
@@ -1,8 +0,0 @@
-// menu
-
-let linksHead = document.querySelectorAll(".linksHead")[0];
-let semaforo = true;
-
-document.querySelectorAll(".burger")[0].addEventListener("click", function(){
-    linksHead.classList.toggle("menudos")
-})
